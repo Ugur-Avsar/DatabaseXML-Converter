e1527fa3a67ca01b658ee03b6a2f05f19c3bb258
LinGur <LinGur@DESKTOP-KJ5AN25>
2017-01-06 20:58:47 +0100

    ugur commit

---
+++ DatabaseXMLConverter/DatabaseXMLConverter/DatabaseConnection.cs
@@ -0,0 +1,109 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+
+namespace DatabaseXMLConverter
+{
+    static class DatabaseConnection
+    {
+        public static string Server
+        {
+            get
+            {
+                return Connection.DataSource;
+            }
+        }
+        public static string Database
+        {
+            get
+            {
+                return Connection.Database;
+            }
+        }
+        public static string ConnectionString
+        {
+            get
+            {
+                return Connection.ConnectionString;
+            }
+        }
+        public static string UserID
+        {
+            get
+            {
+                return new MySqlConnectionStringBuilder(Connection.ConnectionString).UserID;
+            }
+        }
+        public static string Password
+        {
+            get
+            {
+                return new MySqlConnectionStringBuilder(Connection.ConnectionString).Password;
+            }
+        }
+
+        public static MySqlConnection Connection { get; set; }
+        public static MySqlDataAdapter Adapter { get; set; }
+
+        private static MySqlCommand command;
+
+        public static void Connect(string server, string database, string uID, string password)
+        {
+            if (Connection != null && Connection.State != ConnectionState.Closed)
+                Connection.Close();
+            Connection = new MySqlConnection("SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uID + ";" + "PASSWORD=" + password + ";");
+            Connection.Open();
+
+            command = Connection.CreateCommand();
+            Adapter = new MySqlDataAdapter(command.CommandText, Connection);
+        }
+
+        private static void initConnection()
+        {
+            if (Connection != null && Connection.State != ConnectionState.Closed)
+                Connection.Close();
+
+        }
+
+        public static void Close()
+        {
+            if (Connection.State != ConnectionState.Closed)
+            {
+                Connection.Close();
+                Connection = null;
+            }
+        }
+
+        private static List<string> getTableNames()
+        {
+            command.CommandText = "show tables";
+            MySqlDataReader reader = command.ExecuteReader();
+
+            List<string> names = new List<string>();
+            while (reader.Read())
+            {
+                names.Add(reader.GetString(0));
+            }
+            reader.Close();
+            return names;
+        }
+
+        public static List<DataTable> GetTables()
+        {
+            List<DataTable> tables = new List<DataTable>();
+
+            foreach (string tableName in getTableNames())
+            {
+                command.CommandText = "select * from " + tableName;
+                Adapter.SelectCommand = command;
+
+                DataTable c = new DataTable(tableName);
+                Adapter.Fill(c);
+                tables.Add(c);
+            }
+
+            return tables;
+        }
+    }
+}
--- DatabaseXMLConverter/DatabaseXMLConverter/Program.cs
+++ DatabaseXMLConverter/DatabaseXMLConverter/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DatabaseXMLConverter
 {
@@ -10,8 +11,49 @@ namespace DatabaseXMLConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Initial Commit.");
-            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"  _         ___         _          __   _    _                            _                                     
+ | \   /\    |    /\   |_)   /\   (_   |_     )    /  \/  |\/|  |   \    /    _   ._        _   ._  _|_   _   ._
+ |_/  /--\   |   /--\  |_)  /--\  __)  |_    /_    \  /\  |  |  |_  /    \_  (_)  | |  \/  (/_  |    |_  (/_  | 
+                                                                                                                
+");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("by Ugur Avsar & Andreas Erdes");
+
+            Console.ResetColor();
+            Console.Write("\nGeben Sie den Server ein auf dem ihre Datenbank ausgeführt wird: ");
+            string server = Console.ReadLine();
+
+            Console.Write("\nGeben Sie den Namen der Datenbank ein die zu bearbeiten ist: ");
+            string database = Console.ReadLine();
+
+            Console.Write("\nGeben Sie den Benutzernamen ein mit dem auf die Datenbank zugegriffen werden soll: ");
+            string uid = Console.ReadLine();
+
+            Console.Write("\nGeben Sie Ihr Passwort ein: ");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("\n------------------------------------------------");
+            Console.Write("################### OPTIONEN ###################\n" +
+                "Wie sollen Datensätze einer Tabelle benannt werden?\n" +
+                "1.Option: '<Autos_Element .... />'\n" +
+                "2.Option: '<Auto .... />' (Letzter Buchstabe des Tabellennamens wird entfernt)\n" +
+                "'1' oder '2' ... ");
+            int datasetStyle = (int)Char.GetNumericValue(Console.ReadKey(false).KeyChar);
+            Console.WriteLine("\n");
+
+            Console.Write("Output Destination-Path: ");
+            string outputPath = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            DatabaseConnection.Connect(server, database, uid, password);
+
+            outputPath = outputPath.Replace('/', '\\').Replace("\\", "\\\\");
+            XMLConverter.ParseDatabase(DatabaseConnection.Database, DatabaseConnection.GetTables(), XMLConverter.DatasetStyle.ELEMENT).Save(outputPath);
+
+            Process.Start(outputPath);
+
+            DatabaseConnection.Close();
         }
     }
 }
