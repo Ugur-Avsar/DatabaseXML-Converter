19fc576699cfa56a93566d04d1d26d16c392421e
Erdes Andreas <andreas.erdes@gmail.com>
2017-01-06 21:31:27 +0100

    andi commit

--- DatabaseXMLConverter/DatabaseXMLConverter/XMLConverter.cs
+++ DatabaseXMLConverter/DatabaseXMLConverter/XMLConverter.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Data;
+
 
 namespace DatabaseXMLConverter
 {
     class XMLConverter
     {
-        public static void Main(string[] args)
+        public enum DatasetStyle
         {
-            Console.WriteLine("Initial Commit.");
-            Console.ReadKey();
+            CUT, ELEMENT
         }
+
+        public static XElement ParseDatabase(string database, List<DataTable> tables, DatasetStyle style)
+        {
+            XElement db = new XElement(DatabaseConnection.Database, from table in DatabaseConnection.GetTables()
+                    select new XElement(table.TableName, from row in table.Rows.Cast<DataRow>()
+                                        where row.Table.TableName == table.TableName
+                                        select new XElement((style == DatasetStyle.CUT ? (table.TableName.Substring(0, table.TableName.Length - 1)) : (style == DatasetStyle.ELEMENT ? (table.TableName + "_element") : (table.TableName + "_element"))), 
+                                            from attr in table.Columns.Cast<DataColumn>()
+                                            select new XAttribute(attr.ColumnName, row[attr.ColumnName])
+                                        )
+                                )
+                        );
+            return db;
+        }
+
     }
 }
